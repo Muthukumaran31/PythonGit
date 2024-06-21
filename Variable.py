e235112fe55e5970a64b1bf36863a08d02322cd2
Muthukumaran31 <muthukumarand08@gmail.com>
2024-06-21 11:57:19 +0530

    Added more data in variable file

--- Variable.py
+++ Variable.py
@@ -1,9 +1,36 @@
+#For adding values in variable
 a=1
 b="muthu"
 print(a)
 print(b)
 
+#for adding two integer values
 a=int(1)
 b=int(2)
 c=a+b
 print(c)
+
+#for checking the string type of values in the variable
+d="Muthu"
+print(type(d))
+
+#for checking the integer type of values in the variable
+e=12333
+print(type(e))
+
+#for checking the float type of values in the variable
+f=12.333
+print(type(f))
+
+#for checking the Boolean type of values in the variable
+g=True
+print(type(g))
+
+Myvariable=1
+print(Myvariable)
+
+myVariable=2
+print(myVariable)
+
+My_Variable=3
+print(My_Variable)
